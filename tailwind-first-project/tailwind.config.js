ecebeea317b1f75f683721298b6f3f8d6e908c9e
Andra <andra@Atypicals-MacBook-Pro.local>
2023-08-17 16:33:22 +0300

    Tailwind-first-project

---
+++ tailwind-first-project/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [];
+export const theme = {
+  extend: {
+    colors: {
+      primary: '#FF6363',
+      secondary: {
+        100: '#E2E2D5',
+        200: '#888883',
+      }
+    },
+    fontFamily: {
+      body: ['Montserrat'],
+    }
+  },
+};
+export const plugins = [];
+
